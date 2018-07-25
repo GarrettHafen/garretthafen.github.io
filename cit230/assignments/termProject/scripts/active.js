5b0cba37fd1d5cd08f1262fb3811979b03d52a91
Garrett Hafen <garrett.hafen12@gmail.com>
2018-07-25 12:03:50 -0600

    finished hamburger

--- cit230/assignments/termProject/scripts/active.js
+++ cit230/assignments/termProject/scripts/active.js
@@ -1,13 +1,16 @@
 /*-----------------------display active tab-----------------------*/
 
+// Look for #nav
+	var actualNav = document.getElementById("nav");
 // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
+	var hamburger = document.querySelector(".hamburger");
+// On click
+	hamburger.addEventListener("click", function() {
+		// Toggle class "is-active"
+		hamburger.classList.toggle("is-active");
+		//toggle class "miniMenu"
+		actualNav.classList.toggle("miniMenu");
+});
 
 /*-----------------------display active tab-----------------------*/
 function addActiveClass() {
