f876a9bbb58d4fafd03733887f66d92f5390c350
garretthafen <garrett.hafen12@gmail.com>
2018-07-25 10:33:44 -0600

    started hamburger menu

--- cit230/assignments/termProject/scripts/active.js
+++ cit230/assignments/termProject/scripts/active.js
@@ -1,4 +1,15 @@
 /*-----------------------display active tab-----------------------*/
+
+// Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
+
+/*-----------------------display active tab-----------------------*/
 function addActiveClass() {
 	var index 			= document.getElementById("index");
 	var sales 			= document.getElementById("sales");
