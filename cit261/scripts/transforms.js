8e8a7c767577d9110280c328ef6a52a805a8c9a0
garretthafen <garrett.hafen12@gmail.com>
2018-10-17 01:07:30 -0600

    worked on transforms

---
+++ cit261/scripts/transforms.js
@@ -0,0 +1,21 @@
+
+var popUp = document.getElementById("popUp");
+var popUp2 = document.getElementById("popUp2");
+
+/*Scale*/
+function addScale(){
+	popUp.classList.toggle("scale");
+}
+/*rotate*/
+function addRotate(){
+	popUp.classList.toggle("rotate");
+}
+/*skew*/
+function addSkew(){
+	popUp.classList.toggle("skew");
+}
+
+/*all*/
+function addAll(){
+	popUp2.classList.toggle("test");
+}
