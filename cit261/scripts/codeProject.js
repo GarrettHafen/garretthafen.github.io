479ad9d06b5add3b3b2112e51336abefdbb16e18
garretthafen <garrett.hafen12@gmail.com>
2018-12-15 01:51:18 -0700

    improved form validation

--- cit261/scripts/codeProject.js
+++ cit261/scripts/codeProject.js
@@ -146,8 +146,8 @@ function insertSound(sound) {
 //if text box value is empty change border
 
 function validateForm(){
-	var info = {};
 
+	var info = {};
 	var name = document.getElementById("name").value;
 	var channel = document.getElementById("channel").value;
 	var email = document.getElementById("email").value;
@@ -162,11 +162,13 @@ function validateForm(){
 
 	for(var index in info){
 		for(var index in info){
+
 			if(info[index]===""){
 				document.getElementById(index).classList.add("wrong");
 				document.getElementById(index).classList.add("wiggle");
 			}else{
 				document.getElementById(index).classList.remove("wrong");
+				document.getElementById(index).classList.remove("wiggle");
 			}
 		}	
 	}
@@ -237,4 +239,29 @@ function removeThankYou(){
 //flip the thank you card before removing
 function flip(){
 	document.getElementById("thankYou").style.transform = "rotateY(180deg)";
+}
+
+//reset errors to wiggle each time
+function resetErrors(){
+	var resetStuff = document.getElementsByClassName("wiggle");
+	var IdStore = new Array();
+	if(resetStuff.length != 0){
+		for(var i =0; i< resetStuff.length; i++){
+			IdStore[i] = resetStuff[i].id;
+		}
+
+		for (var j=0; j<IdStore.length; j++){
+			document.getElementById(IdStore[j]).classList.remove("wiggle");
+			document.getElementById(IdStore[j]).classList.remove("wrong");
+			
+		}
+		setTimeout(function() { validateForm(); }, 50);
+	}else{
+		validateForm();
+	}
+}
+
+//single validate when leaving input to clear red box
+function singleValidation(id){
+	document.getElementById(id).classList.remove("wrong");
 }
